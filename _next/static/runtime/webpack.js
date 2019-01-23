70cba1d8729cf638a76546d19c8a2b3d8d23c3a8
alihsan <al.ihsan@docotel.com>
2019-01-23 23:16:09 +0800

    update baru mantap

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a76cbb40247d461161f5";
+/******/ 	var hotCurrentHash = "eac12e1496be72142fc7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
